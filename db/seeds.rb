fdd30e0b88e73edf228951e708072a28e7078506
upuld <u.dissanayaka@gmail.com>
2017-09-02 23:51:12 +0100

    Seeded ForecastGeneratedAt & tested save method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require_relative("../models/location")
 require_relative("../models/weather_variable")
+require_relative("../models/forecast_generated_at")
 
 location1 = Location.new({
   "name" => "Findhorn"
@@ -22,4 +23,19 @@ weather_variable2 = WeatherVariable.new({
 
 weather_variable1.save()
 weather_variable2.save()
+
+forecast_generated_at1 = ForecastGeneratedAt.new({
+  "time_stamp" => 1504392371,
+  "location_id" => location1.id,
+  "weather_variable_id" => weather_variable1.id
+  })
+
+forecast_generated_at2 = ForecastGeneratedAt.new({
+  "time_stamp" => 1504392483,
+  "location_id" => location2.id,
+  "weather_variable_id" => weather_variable2.id
+  })
+
+forecast_generated_at1.save()
+forecast_generated_at2.save()
 
