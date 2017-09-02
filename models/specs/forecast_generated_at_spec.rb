cb0505e1c26a0dbfe679b64edc86a7e47fafb69c
upuld <u.dissanayaka@gmail.com>
2017-09-02 21:05:44 +0100

    ForecastGeneratedAt tested for location_id

--- models/specs/forecast_generated_at_spec.rb
+++ models/specs/forecast_generated_at_spec.rb
@@ -17,4 +17,8 @@ class ForecastGeneratedAtTest < Minitest::Test
     assert_equal(1504382072, @forecast_generated_at.time_stamp)
   end
 
+  def test_forecast_generated_at_location_id
+    assert_equal(1, @forecast_generated_at.location_id)
+  end
+
 end
