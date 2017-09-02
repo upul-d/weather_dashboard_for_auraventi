38ef1f7fb526e313f6e01b76e1e062485b75202c
upuld <u.dissanayaka@gmail.com>
2017-09-02 21:03:47 +0100

    ForecastGeneratedAt tested for time_stamp

--- models/specs/forecast_generated_at_spec.rb
+++ models/specs/forecast_generated_at_spec.rb
@@ -13,4 +13,8 @@ class ForecastGeneratedAtTest < Minitest::Test
     assert_equal(4, @forecast_generated_at.id)
   end
 
+  def test_forecast_generated_at_time_stamp
+    assert_equal(1504382072, @forecast_generated_at.time_stamp)
+  end
+
 end
