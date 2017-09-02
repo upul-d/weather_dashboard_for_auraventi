84430516f5365ddaaed0115d18be92cf11acf145
upuld <u.dissanayaka@gmail.com>
2017-09-02 21:47:42 +0100

    Forecast tested for time_GMT

--- models/specs/forecast_spec.rb
+++ models/specs/forecast_spec.rb
@@ -21,4 +21,8 @@ class ForecastTest < Minitest::Test
     assert_equal(1504382072, @forecast.time_stamp)
   end
 
+  def test_forecast_time_GMT
+    assert_equal("9:00 PM 9/2/17", @forecast.time_GMT)
+  end
+
 end
