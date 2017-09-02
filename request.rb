bba70149f2e355d8659d88a9fdfc0078016cd1c6
upuld <u.dissanayaka@gmail.com>
2017-09-02 10:38:04 +0100

    Refactored getBody params and output method

--- request.rb
+++ request.rb
@@ -1,30 +1,29 @@
 require 'net/http'
 
-class Request
-  
-  def getBody(length)
-    uri = URI('https://api.auraventi.com/forecast/weather/temperature/findhorn')
-    params = length
-    uri.query = URI.encode_www_form(params)
+class Request  
+  def getBody(predictionType, location, queryParms)
+    uri = URI("https://api.auraventi.com/forecast/weather/#{predictionType}/#{location}")
+    if(queryParms!= nil)
+      uri.query = URI.encode_www_form(queryParms)
+    end
 
     # Creates a new http object
     # Connects to server
+    resBody = ""
+    resCode = 0
     Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
       request = Net::HTTP::Get.new uri
       request['Accept'] = "application/json"
       request['Authorization'] = "Basic dXB1bDota0FHYyNjWCZkdFRYWnR1JQ=="
 
       res = http.request(request)
-      # puts res.code
       resBody = res.body if res.is_a?(Net::HTTPSuccess)
+      resCode = res.code
 
-      open('response.txt', 'w') do |f|
-        f.puts resBody
-      end
-      # return res.body
+      # Ends connection to server
+      http.finish
     end
+    puts "Server returned HTTP status #{resCode}"
+    return resBody
   end
-
 end
-
-Request.new().getBody({:length => 4});
