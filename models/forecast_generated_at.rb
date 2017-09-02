f77816fd4924ecc48cd4980c5d9604c6efbab582
upuld <u.dissanayaka@gmail.com>
2017-09-02 23:42:54 +0100

    Added save method to ForecastGeneratedAt

--- models/forecast_generated_at.rb
+++ models/forecast_generated_at.rb
@@ -1,4 +1,4 @@
-# require_relative("../db/sql_runner")
+require_relative("../db/sql_runner")
 
 class ForecastGeneratedAt
 
@@ -9,6 +9,12 @@ attr_reader :id, :time_stamp, :location_id, :weather_variable_id
     @time_stamp = options["time_stamp"].to_i if options["time_stamp"]
     @location_id = options["location_id"].to_i if options["location_id"]
     @weather_variable_id = options["weather_variable_id"].to_i if options["weather_variable_id"]
+  end
+
+  def save()
+    sql = "INSERT INTO forecasts_generated_at (time_stamp, location_id, weather_variable_id) VALUES (#{@time_stamp}, #{@location_id}, #{@weather_variable_id}) RETURNING id"
+    forecast_generated_at = SqlRunner.run(sql).first
+    @id = forecast_generated_at["id"].to_i
   end
 
 end
