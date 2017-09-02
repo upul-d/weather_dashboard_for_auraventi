96aabf89350116c2c8e86a41b259f269f12cfc9a
upuld <u.dissanayaka@gmail.com>
2017-09-02 19:18:24 +0100

    Location tested for name

---
+++ models/specs/location_spec.rb
@@ -0,0 +1,16 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../location")
+
+class LocationTest < Minitest::Test
+
+  def setup
+    test_location = {"id" => 8, "name" => "Edinburgh"}
+    @location = Location.new(test_location)
+  end
+
+  def test_location_name
+    assert_equal("Edinburgh", @location.name)
+  end
+
+end
