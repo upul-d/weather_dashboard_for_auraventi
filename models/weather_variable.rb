721f9c7838f9494d272a0585986e409db156aee0
upuld <u.dissanayaka@gmail.com>
2017-09-02 23:18:56 +0100

    Added save method to WeatherVariable

--- models/weather_variable.rb
+++ models/weather_variable.rb
@@ -1,4 +1,4 @@
-# require_relative("../db/sql_runner")
+require_relative("../db/sql_runner")
 
 class WeatherVariable
 
@@ -7,6 +7,12 @@ attr_reader :id, :name
   def initialize(options)
     @id = options["id"].to_i if options["id"]
     @name = options["name"]
+  end
+
+  def save()
+    sql = "INSERT INTO weather_variables (name) VALUES ('#{@name}') RETURNING id"
+    weather_variable = SqlRunner.run(sql).first
+    @id = weather_variable["id"].to_i
   end
 
 end
