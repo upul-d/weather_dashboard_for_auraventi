6d4f541218e9985d7633ae9dec5a50e4f983d140
upuld <u.dissanayaka@gmail.com>
2017-09-03 19:45:38 +0100

    Improved FGA save method to account for duplicates

--- models/forecast_generated_at.rb
+++ models/forecast_generated_at.rb
@@ -4,17 +4,22 @@ class ForecastGeneratedAt
 
 attr_reader :id, :time_stamp, :location_id, :weather_variable_id
 
-  def initialize(options)
-    @id = options["id"].to_i if options["id"]
-    @time_stamp = options["time_stamp"].to_i if options["time_stamp"]
-    @location_id = options["location_id"].to_i if options["location_id"]
-    @weather_variable_id = options["weather_variable_id"].to_i if options["weather_variable_id"]
+  def initialize(fga_hash)
+    @id = fga_hash["id"].to_i if fga_hash["id"]
+    @time_stamp = fga_hash["time_stamp"].to_i if fga_hash["time_stamp"]
+    @location_id = fga_hash["location_id"].to_i if fga_hash["location_id"]
+    @weather_variable_id = fga_hash["weather_variable_id"].to_i if fga_hash["weather_variable_id"]
   end
 
   def save()
-    sql = "INSERT INTO forecasts_generated_at (time_stamp, location_id, weather_variable_id) VALUES (#{@time_stamp}, #{@location_id}, #{@weather_variable_id}) RETURNING id"
-    forecast_generated_at = SqlRunner.run(sql).first
-    @id = forecast_generated_at["id"].to_i
+    sql = "SELECT id, time_stamp, location_id, weather_variable_id from forecasts_generated_at WHERE (time_stamp=#{@time_stamp} AND location_id=#{@location_id} AND weather_variable_id=#{@weather_variable_id})"
+    existingFGAHash = SqlRunner.run(sql).first
+    if(existingFGAHash == nil)
+      sql = "INSERT INTO forecasts_generated_at (time_stamp, location_id, weather_variable_id) VALUES (#{@time_stamp}, #{@location_id}, #{@weather_variable_id}) RETURNING id"
+      fga = SqlRunner.run(sql).first
+      @id = fga["id"].to_i
+    else
+      @id = existingFGAHash["id"].to_i
+    end
   end
-
 end
