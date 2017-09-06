a33d1c3aa330338c87e26a44cecac16f0e824ac7
upuld <u.dissanayaka@gmail.com>
2017-09-06 01:43:48 +0100

    Added FGA class method: find

--- models/forecast_generated_at.rb
+++ models/forecast_generated_at.rb
@@ -2,7 +2,7 @@ require_relative("../db/sql_runner")
 
 class ForecastGeneratedAt
 
-attr_reader :id, :time_stamp, :location_id, :weather_variable_id
+  attr_reader :id, :time_stamp, :location_id, :weather_variable_id
 
   def initialize(fga_hash)
     @id = fga_hash["id"].to_i if fga_hash["id"]
@@ -21,5 +21,34 @@ attr_reader :id, :time_stamp, :location_id, :weather_variable_id
     else
       @id = existingFGAHash["id"].to_i
     end
+  end
+
+  def self.find(location, weatherVariable)
+    #Find the location ID using the location name
+    forecastLocation = Location.find(location)
+    if(forecastLocation != nil)
+      forecastLocationId = forecastLocation["id"].to_i
+    else
+      forecastLocationId = nil
+    end
+
+    #Find the weather variable ID using the weather variable name
+    forecastWeatherVariable = WeatherVariable.find(weatherVariable)
+    if(forecastWeatherVariable != nil)
+      forecastWeatherVariableId = forecastWeatherVariable["id"].to_i
+    else
+      forecastWeatherVariableId = nil
+    end
+
+    #Get the rounded down timestamp
+    fgaTimeStamp = Time.now.hour.to_i
+
+    #Now look in the DB for a match
+    if(forecastLocationId  != nil && forecastWeatherVariableId != nil)
+      sql = "SELECT id, time_stamp, location_id, weather_variable_id from forecasts_generated_at WHERE (time_stamp=#{fgaTimeStamp} AND location_id=#{forecastLocationId} AND weather_variable_id=#{forecastWeatherVariableId})"
+      forecastFGAHash = SqlRunner.run(sql).first
+      return forecastFGAHash
+    end
+    return nil
   end
 end
